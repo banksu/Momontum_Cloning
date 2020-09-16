36971fb83eaa06a9a448118f8e902441ab06e757
banksu <67863190+banksu@users.noreply.github.com>
2020-09-16 13:24:46 +0900

    Momontum Cloning #3.5a

--- todo.js
+++ todo.js
@@ -1,9 +1,36 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = form.querySelector("input"),
-  toDoList = document.querySelector("js-toDoList");
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function panintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  panintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
 
 function init() {
   loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
